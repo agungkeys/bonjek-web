4763cc09f1dda89ae499e3db11724a632d337973
Agung Kurniawan <agung.kurniawan@sehatq.com>
2021-08-17 16:16:19 +0700

    add config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
   images: {
     domains: ['localhost', 'res.cloudinary.com'],
   },
+  publicRuntimeConfig: {
+    API: process.env.BASE_API,
+    ENV: process.env.BASE_ENV,
+    HOST: process.env.BASE_HOST,
+  },
 }
