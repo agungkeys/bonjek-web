ee443a743f03312720f2dc002354e42ca29c389f
Agung Kurniawan <agung.kurniawan@sehatq.com>
2021-08-17 11:34:30 +0700

    add config env

--- constants/env.js
+++ constants/env.js
@@ -7,12 +7,10 @@ const CURRENT_HOST = publicRuntimeConfig.HOST || null;
 const BASE_URL = CURRENT_HOST;
 const LOGO = `https://res.cloudinary.com/dsxlujoww/image/upload/v1628741405/bonjek_84c7ea5d2e.svg`;
 
-const ENV = {
+export default ENV = {
   CURRENT_ENV,
   CURRENT_API,
   CURRENT_HOST,
   BASE_URL,
   LOGO,
 }
-
-export default ENV;
